0b545ca820d18a622ad84e75e81f04c0b02aa6ca
dippingconda <qkr514236~>
2020-08-02 14:51:49 +0900

    solved

--- two-sum.py
+++ two-sum.py
@@ -13,7 +13,13 @@ def two_sum(nums, target):
             return idx, nums_dict[target - val]
 
 
+def two_sum2(nums, target):
+    for val in nums:
+        if target - val in nums:
+            return nums.index(val), nums.index(target - val)
+
+
 if __name__ == '__main__':
     nums = [2, 7, 11, 15]
     target = 9
-    print(f'{two_sum(nums, target)}')
+    print(f'{two_sum2(nums, target)}')

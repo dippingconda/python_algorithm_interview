9dccb8829ee33925f579e496cba6a7f73b52aa1b
dippingconda <qkr514236~>
2020-08-02 16:39:48 +0900

    solved

--- two-sum.py
+++ two-sum.py
@@ -1,9 +1,9 @@
 """
 https://leetcode.com/problems/two-sum
 """
+from typing import List, Tuple
 
-
-def two_sum(nums, target):
+def two_sum(nums: List[int], target: int) -> Tuple[int, int]:
     nums_dict = {}
     for idx, val in enumerate(nums):
         nums_dict[val] = idx
@@ -13,7 +13,7 @@ def two_sum(nums, target):
             return idx, nums_dict[target - val]
 
 
-def two_sum2(nums, target):
+def two_sum2(nums: List[int], target: int) -> Tuple[int, int]:
     for val in nums:
         if target - val in nums:
             return nums.index(val), nums.index(target - val)
@@ -22,4 +22,5 @@ def two_sum2(nums, target):
 if __name__ == '__main__':
     nums = [2, 7, 11, 15]
     target = 9
+    print(f'{two_sum(nums, target)}')
     print(f'{two_sum2(nums, target)}')

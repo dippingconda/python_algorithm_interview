550d564146a18a4708b5f413ba2763e4e0400f2b
dippingconda <qkr514236~>
2020-08-02 14:46:11 +0900

    solved

---
+++ two-sum.py
@@ -0,0 +1,19 @@
+"""
+https://leetcode.com/problems/two-sum
+"""
+
+
+def two_sum(nums, target):
+    nums_dict = {}
+    for idx, val in enumerate(nums):
+        nums_dict[val] = idx
+
+    for idx, val in enumerate(nums):
+        if target - val in nums_dict and idx != nums_dict[target - val]:
+            return idx, nums_dict[target - val]
+
+
+if __name__ == '__main__':
+    nums = [2, 7, 11, 15]
+    target = 9
+    print(f'{two_sum(nums, target)}')
